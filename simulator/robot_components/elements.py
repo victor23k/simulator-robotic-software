95eb77ed808ef5a98e8db46f11938cbbe50c8f88
UO270437 <UO270437@uniovi.es>
2023-04-11 09:37:16 +0200

    Añadidas clases para todos los elementos

--- simulator/robot_components/elements.py
+++ simulator/robot_components/elements.py
@@ -213,3 +213,562 @@ class UltrasoundSensor(Element):
         pin
         """
         return "digital"
+
+
+class ResistanceArduino(Element):
+    def __init__(self):
+        """
+        Constructor for resistance
+        A resistance has two pins and a value
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.value = 10
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "both"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "both"
+
+    def set_value(self, value):
+        self.value = value
+
+
+class ButtonArduino(Element):
+    def __init__(self):
+        """
+        Constructor for button
+        A button has four pins and a state
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.pin4 = -1
+        self.state = True
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "both"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "both"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "both"
+
+    def get_pin4_type(self):
+        """
+        Returns connection type needed for the pin4
+        """
+        return "both"
+
+    def open(self):
+        """Open the button"""
+        self.state = False
+
+    def close(self):
+        """Close the button"""
+        self.state = True
+
+
+class PotentiometerArduino(Element):
+    def __init__(self):
+        """
+        Constructor for potentiometer
+        A potentiometer has three pins and a value
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.value = 100
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "both"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "both"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "both"
+
+    def set_value(self, value):
+        """The value must be between 0 and 100"""
+        if 0 <= value <= 100:
+            self.value = value
+
+
+class LedArduino(Element):
+    def __init__(self):
+        """
+        Constructor for LED
+        A LED has two pins, a state and a color
+        - Color: 1 for red, 2 for yellow and 3 for green
+            In future expansions, more colors could be added
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.state = False
+        self.color = 1
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "both"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "both"
+
+    def set_on(self):
+        """Turns on the LED"""
+        self.state = True
+
+    def set_off(self):
+        """Turns off the LED"""
+        self.state = False
+
+    def set_color(self, color):
+        """For now there are only three colors"""
+        if color == 1 or color == 2 or color == 3:
+            self.color = color
+
+
+class BuzzerArduino(Element):
+    def __init__(self):
+        """
+        Constructor for buzzer
+        A buzzer has three pins and a state
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.state = False
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "both"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "both"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "both"
+
+    def set_on(self):
+        """Turns on the buzzer"""
+        self.state = True
+
+    def set_off(self):
+        """Turns off the buzzer"""
+        self.state = False
+
+
+class RGBArduino(Element):
+    def __init__(self):
+        """
+        Constructor for LED RGB
+        A LED RGB has four pins and a value
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.pin4 = -1
+        self.value = 1
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def get_pin4_type(self):
+        """
+        Returns connection type needed for the pin4
+        """
+        return "both"
+
+    def set_value(self, value):
+        """Sets the value of the LED to a certain value"""
+        self.value = value
+
+
+class LightSensorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for Light Sensor
+        A Light sensor has three pins and a sensor
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.sensor = False
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "analog"
+
+    def set_sensor_on(self):
+        """Turns the sensor on"""
+        self.sensor = True
+
+    def set_sensor_off(self):
+        """Turns the sensor off"""
+        self.sensor = False
+
+
+class PIRSensorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for PIR Sensor
+        A PIR sensor has three pins and a sensor
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.sensor = False
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def set_sensor_on(self):
+        """Turns the sensor on"""
+        self.sensor = True
+
+    def set_sensor_off(self):
+        """Turns the sensor off"""
+        self.sensor = False
+
+
+class VibrationSensorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for Vibration Sensor
+        A Vibration sensor has three pins and a sensor
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.sensor = True
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def set_sensor_on(self):
+        """Turns the sensor on"""
+        self.sensor = True
+
+    def set_sensor_off(self):
+        """Turns the sensor off"""
+        self.sensor = False
+
+
+class InfraredSensorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for Infrared Sensor
+        An Infrared sensor has three pins and a sensor
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.sensor = True
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def set_sensor_on(self):
+        """Turns the sensor on"""
+        self.sensor = True
+
+    def set_sensor_off(self):
+        """Turns the sensor off"""
+        self.sensor = False
+
+
+class UltrasoundSensorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for Ultrasound Sensor
+        An Ultrasound sensor has four pins and a sensor
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.pin4 = -1
+        self.sensor = True
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def get_pin4_type(self):
+        """
+        Returns connection type needed for the pin4
+        """
+        return "digital"
+
+    def set_sensor_on(self):
+        """Turns the sensor on"""
+        self.sensor = True
+
+    def set_sensor_off(self):
+        """Turns the sensor off"""
+        self.sensor = False
+
+
+class KeyBoardArduino(Element):
+    def __init__(self):
+        """
+        Constructor for keyboard
+        A keyboard has eight pins
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.pin4 = -1
+        self.pin5 = -1
+        self.pin6 = -1
+        self.pin7 = -1
+        self.pin8 = -1
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def get_pin4_type(self):
+        """
+        Returns connection type needed for the pin4
+        """
+        return "digital"
+
+    def get_pin5_type(self):
+        """
+        Returns connection type needed for the pin5
+        """
+        return "digital"
+
+    def get_pin6_type(self):
+        """
+        Returns connection type needed for the pin6
+        """
+        return "digital"
+
+    def get_pin7_type(self):
+        """
+        Returns connection type needed for the pin7
+        """
+        return "digital"
+
+    def get_pin8_type(self):
+        """
+        Returns connection type needed for the pin8
+        """
+        return "digital"
+
+
+class ScreenArduino(Element):
+    def __init__(self):
+        """
+        Constructor for Screen
+        A screen has four pins
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.pin4 = -1
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def get_pin4_type(self):
+        """
+        Returns connection type needed for the pin4
+        """
+        return "digital"
+
+
+class ServomotorArduino(Element):
+    def __init__(self):
+        """
+        Constructor for servomotor
+        A servomotor has three pins and a state
+        """
+        self.pin1 = -1
+        self.pin2 = -1
+        self.pin3 = -1
+        self.state = False
+
+    def get_pin1_type(self):
+        """
+        Returns connection type needed for the pin1
+        """
+        return "digital"
+
+    def get_pin2_type(self):
+        """
+        Returns connection type needed for the pin2
+        """
+        return "digital"
+
+    def get_pin3_type(self):
+        """
+        Returns connection type needed for the pin3
+        """
+        return "digital"
+
+    def set_on(self):
+        """Turns on the servomotor"""
+        self.state = True
+
+    def set_off(self):
+        """Turns off the servomotor"""
+        self.state = False
