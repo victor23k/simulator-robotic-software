c55726092d63c950893d6960af81a1b778319bc1
victor23k <victorfdez.f@gmail.com>
2025-05-11 11:26:52 +0200

    Refactor environment

--- simulator/interpreter/environment.py
+++ simulator/interpreter/environment.py
@@ -1,43 +1,64 @@
-from enum import Enum
 from dataclasses import dataclass
-from simulator.interpreter.token import Token, TokenType
+from typing import Self
 
-
-class EnvError(Enum):
-    Redefinition = 1
-    VariableDoesNotExist = 2
+from simulator.interpreter.types import ArduinoType
+from simulator.interpreter.token import Token
 
 
 @dataclass
 class Value:
-    value_type: TokenType
+    value_type: ArduinoType
     value: object
 
 
 class Environment:
     """
     Storage for variables and their values.
+
+    An environment can have an enclosing environment. This forms a tree
+    structure where each scope has an environment where its variables can live
+    in.
     """
 
-    values: dict[str, Value]
+    values: dict[str, Value | None]
+    enclosing: Self | None
 
-    def __init__(self):
+    def __init__(self, enclosing: Self | None):
         self.values = {}
+        self.enclosing = enclosing
 
-    def define(self, name: Token, value: Value) -> None | EnvError:
+    def define(self, name: Token, value: Value | None):
+        """
+        Defines a variable by its `name` and `value`.
+
+        If a variable should be defined but not initialized, use `None` as the
+        `value`.
+        """
+
+        # this should not happen, checked in the Resolver
+        assert name not in self.values
+
+        self.values[name.lexeme] = value
+
+    def assign(self, name: Token, value: Value | None):
         if name in self.values:
-            return EnvError.Redefinition
+            self.values[name.lexeme] = value
+            return
 
-        self.values[name.lexeme] = value
+        if self.enclosing:
+            self.enclosing.assign(name, value)
+            return
 
-    def set(self, name: Token, value: Value) -> None | EnvError:
-        if name not in self.values:
-            return EnvError.VariableDoesNotExist
+        # should be unreachable, checked in the Resolver
+        return None
 
-        self.values[name.lexeme] = value
 
-    def get(self, name: Token) -> Value | EnvError:
-        if name not in self.values:
-            return EnvError.VariableDoesNotExist
+    def get(self, name: Token) -> Value | None:
+        if name in self.values:
+            return self.values[name.lexeme]
 
-        return self.values[name.lexeme]
+        if self.enclosing:
+            return self.enclosing.get(name)
+
+        # should be unreachable, checked in the Resolver
+        return None
