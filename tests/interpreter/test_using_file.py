44528d2db98f641ab5a6a1bef3defcb69f1da369
victor23k <victorfdez.f@gmail.com>
2025-06-23 20:46:33 +0200

    extend files test
    Checks input files under `tests/interpreter/inputs/` against expected output files
    under `tests/interpreter/outputs/`.
    Adds failures tests for input files is expected to contain errors.

--- tests/interpreter/test_using_file.py
+++ tests/interpreter/test_using_file.py
@@ -1,10 +1,15 @@
+import os
+
+from simulator.interpreter.interpreter import Interpreter
 from simulator.interpreter.parser import Parser
+from simulator.interpreter.resolver import Resolver
 from simulator.interpreter.token import TokenType
 from tests.interpreter.ast import (
     BinaryExprSpec,
     ExpressionStmtSpec,
     ExprSpec,
     LiteralExprSpec,
+    StmtSpec,
     TokenSpec,
     VariableExprSpec,
     VariableStmtSpec,
@@ -13,11 +18,12 @@ from tests.interpreter.ast import (
 
 def match_structure(actual: object, spec: object):
     if isinstance(spec, list):
-        return (isinstance(actual, list) and 
-            all(match_structure(a, s) for a, s in zip(actual, spec)))
-    elif (hasattr(spec, '__dict__') and 
-         (str.rstrip(spec.__class__.__name__, "Spec") ==
-         actual.__class__.__name__)):
+        return isinstance(actual, list) and all(
+            match_structure(a, s) for a, s in zip(actual, spec)
+        )
+    elif hasattr(spec, "__dict__") and (
+        str.rstrip(spec.__class__.__name__, "Spec") == actual.__class__.__name__
+    ):
         for key, val in spec.__dict__.items():
             if not hasattr(actual, key):
                 return False
@@ -29,14 +35,56 @@ def match_structure(actual: object, spec: object):
     else:
         return spec is None or spec == actual
 
-with open("./tests/interpreter/outputs/variable_assignment.ir", "r") as ir_file:
-    ir = ir_file.read()
 
-with open("./tests/interpreter/inputs/variable_assignment.ino", "r") as ino_file:
-    code = ino_file.read()
+input_test_dir = "./tests/interpreter/inputs/"
+output_test_dir = "./tests/interpreter/outputs/"
+input_test_filenames = os.listdir(input_test_dir)
+output_test_filenames = os.listdir(output_test_dir)
+test_failures_filenames = [filename for filename in input_test_filenames if
+    filename.rfind("_fails") != -1]
 
-parser = Parser(code, [])
-stmts = parser.parse()
+input_test_filenames = set(input_test_filenames).difference(test_failures_filenames)
 
-spec_stmts = eval(ir)
-print(match_structure(stmts, spec_stmts))
+for input_filename in input_test_filenames:
+    input_file_path = os.path.join(input_test_dir, input_filename)
+    with open(input_file_path, "r") as ino_file:
+        code = ino_file.read()
+
+        diags = []
+        parser = Parser(code, diags)
+        stmts = parser.parse()
+
+    output_filename_noext, _ext = os.path.splitext(input_filename)
+    if output_filename_noext + ".ir" not in output_test_filenames:
+        raise Exception(f"No expected output file for test '{input_filename}' found.")
+
+    output_file_path = os.path.join(output_test_dir, output_filename_noext + ".ir")
+
+    with open(output_file_path, "r") as ir_file:
+        ir = ir_file.read()
+        spec_stmts: list[StmtSpec] = eval(ir)
+
+    if not match_structure(stmts, spec_stmts):
+        raise Exception(f"Parser error in test case '{input_filename}'")
+
+    interpreter = Interpreter(stmts, diags)
+    resolver = Resolver(interpreter)
+    resolver.resolve(stmts)
+
+    if len(diags) > 0:
+        raise Exception(f"Error in test case '{input_filename}'. {diags}")
+
+for failure_filename in test_failures_filenames:
+    input_file_path = os.path.join(input_test_dir, failure_filename)
+    with open(input_file_path, "r") as ino_file:
+        code = ino_file.read()
+        diags = []
+
+        parser = Parser(code, diags)
+        stmts = parser.parse()
+        interpreter = Interpreter(stmts, diags)
+        resolver = Resolver(interpreter)
+        resolver.resolve(stmts)
+
+        if len(diags) == 0:
+            raise Exception(f"Error in test case '{failure_filename}'")
