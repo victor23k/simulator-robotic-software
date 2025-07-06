c02d130c37c80b5735f0f8bd65a942d4dc1ee10e
victor23k <victorfdez.f@gmail.com>
2025-07-06 13:05:51 +0200

    Implement entrypoint for interpreter and CLI
    The Arduino module is the API for the interpreter.
    The CLI can run and check arduino sketches.
    The CLI is installed as `interpino`.

---
+++ simulator/interpreter/cli.py
@@ -0,0 +1,47 @@
+import argparse
+import sys
+
+from simulator.interpreter.arduino import Arduino
+
+
+def main():
+    """Command Line Interface of the Arduino Interpreter."""
+
+    parser = argparse.ArgumentParser(prog="interpino", description=main.__doc__)
+
+    parser.add_argument("command", type=str, choices=["run", "check", "debug"])
+    parser.add_argument(
+        "-f",
+        "--filename",
+        default=sys.stdin,
+        type=argparse.FileType("r"),
+        dest="file",
+        help="Path to file containing the Arduino sketch source code. If no path is specified, tries to read from stdin",
+    )
+
+    args = parser.parse_args()
+
+    if args.file.isatty():
+        parser.error(message="No filename was specified and stdin is empty.")
+
+    code = args.file.read()
+
+    arduino = Arduino(code)
+
+    if args.command == "run":
+        arduino.run()
+
+    elif args.command == "check":
+        if arduino.check_program():
+            print("All good!!")
+        else:
+            print("Found some errors:")
+            arduino.print_diagnostics()
+
+    elif args.command == "debug":
+        print("Debug not implemented yet")
+        pass
+
+
+if __name__ == "__main__":
+    main()
